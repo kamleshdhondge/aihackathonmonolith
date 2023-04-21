2edbfff3384467e952add36d9fa23d5a00c06a3a
Kamlesh Kailas Dhondge <kamlesh.dhondge@gmail.com>
2023-04-21 21:57:00 +0000

    UI working

--- frontend/src/components/Main.js
+++ frontend/src/components/Main.js
@@ -1,18 +1,47 @@
-import './Main.css'
+import "./Main.css";
+import Grid from "@mui/material/Grid";
+
+
 
 const LeftSideView = () => {
-    return <div className='column'>Here is your report mate</div>
-}
+  return (
+    <>
+      <Grid
+        container
+        direction="column"
+        justifyContent="center"
+        alignItems="center"
+      >
+        <Grid item xs={6}>
+          <div>1</div>
+        </Grid>
+        <Grid item xs={6}>
+          <div>2</div>
+        </Grid>
+        <Grid item xs={6}>
+          <div>3</div>
+        </Grid>
+        <Grid item xs={6}>
+          <div>4</div>
+        </Grid>
+      </Grid>
+    </>
+  );
+};
 
 const RightSideView = () => {
-   return <div className='column'>
-       <div className='column'>Chat with me man !!!</div>
-   </div>
-}
+  return (
+    <div className="column">
+      <div className="column">Chat with me man !!!</div>
+    </div>
+  );
+};
 
 export const Main = (props) => {
-    return <div className='container'>
-        <LeftSideView />
-        <RightSideView />
+  return (
+    <div className="container">
+      <LeftSideView />
+      <RightSideView />
     </div>
-}
+  );
+};
