885c8e3b1ff3867a1e1f771dcef8d941cb749e7c
Juan Sebastian Prieto <js.prieto10@uniandes.edu.co>
2023-04-21 22:47:51 +0000

    summary

--- backend/routes/document_endpoint.js
+++ backend/routes/document_endpoint.js
@@ -9,7 +9,8 @@ router.route("/:id/chat").get(async (req, res) => {
 });
 
 router.route("/:id/summary").get(async (req, res) => {
-  res.json({ message: `${req.params.id}` });
+  let text = getDocument(req.params.id);
+  res.json({text});
 });
 
 router.route("/:id/flags").get(async (req, res) => {
