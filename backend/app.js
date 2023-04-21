1d018a022d2a85ddc48abbde8c38fbc9f69263ae
Toño <aajn88@gmail.com>
2023-04-21 21:44:50 +0100

    typo

--- backend/app.js
+++ backend/app.js
@@ -13,5 +13,5 @@ configRoutes(app);
 
 app.listen(5000, () => {
   console.log("We've now got a server!");
-  console.log("Your routes will be running on http://localhost:3000");
+  console.log("Your routes will be running on http://localhost:5000");
 });
