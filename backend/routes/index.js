e33b8ecadf8966fcae62a925e1b640706d7274b5
Sara María Bejarano <sm.bejarano44@uniandes.edu.co>
2023-04-21 23:25:27 +0100

    hardcoding flags and adding contract

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,9 +1,11 @@
 import upload_doc from "./upload.js";
 import chat from "./chat.js";
+import flags from "./flags.js";
 
 const constructorMethod = (app) => {
   app.use("/upload", upload_doc);
   app.use("/document", chat);
+  app.use("/flags", flags);
   app.use("*", (req, res) => {
     res.redirect("/");
   });
