a6a1b03b9b7e53b01c6ff84e85e68648a6c907d8
akhmedelnemer <131448115+akhmedelnemer@users.noreply.github.com>
2023-04-21 22:22:18 +0000

    Merge conflict

--- frontend/src/components/Main.js
+++ frontend/src/components/Main.js
@@ -1,9 +1,32 @@
 import './Main.css'
 import {Chat} from './Chat';
+import Grid from "@mui/material/Grid";
 
 const LeftSideView = () => {
-    return <div className='column'>Here is your report mate</div>
-}
+  return (
+    <>
+      <Grid
+        container
+        direction="column"
+        justifyContent="center"
+        alignItems="center"
+      >
+        <Grid item >
+          <div style={{ backgroundColor: '#f5f5f5', height: 600, width:400, margin:20 }}>Summary</div>
+        </Grid>
+        <Grid item >
+          <div style={{ backgroundColor: 'red', height: 50, width:400, margin:20 }}>Red</div>
+        </Grid>
+        <Grid item >
+          <div style={{ backgroundColor: 'yellow', height: 50, width:400, margin:20 }}>Yello</div>
+        </Grid>
+        <Grid item >
+          <div style={{ backgroundColor: 'green', height: 50, width:400, margin:20 }} >Green</div>
+        </Grid>
+      </Grid>
+    </>
+  );
+};
 
 const RightSideView = () => {
    return <div className='column'>
@@ -12,8 +35,11 @@ const RightSideView = () => {
 }
 
 export const Main = (props) => {
-    return <div className='container'>
-        <LeftSideView />
-        <RightSideView />
+  return (
+    <div className="container">
+      
+      <LeftSideView />
+      <RightSideView />
     </div>
-}
+  );
+};
