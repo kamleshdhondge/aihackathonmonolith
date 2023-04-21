b459bcb909946e98f06a300ce7f034d1f9fb42b3
Toño <aajn88@gmail.com>
2023-04-21 21:44:07 +0100

    changing port

--- backend/app.js
+++ backend/app.js
@@ -11,7 +11,7 @@ import configRoutes from "./routes/index.js";
 
 configRoutes(app);
 
-app.listen(3000, () => {
+app.listen(5000, () => {
   console.log("We've now got a server!");
   console.log("Your routes will be running on http://localhost:3000");
 });
