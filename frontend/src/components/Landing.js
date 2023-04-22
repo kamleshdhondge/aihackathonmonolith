633f823520648a56dcd9aff224322f39bab649bf
kamlesh <arvdigitech@gmail.com>
2023-04-22 02:07:39 +0100

    UI Fix

--- frontend/src/components/Landing.js
+++ frontend/src/components/Landing.js
@@ -1,46 +1,52 @@
-import { useContext } from 'react'
-import './Landing.css'
-import { AppContext } from '../state/AppContext'
-
+import { useContext } from "react";
+import "./Landing.css";
+import { AppContext } from "../state/AppContext";
+import {
+    Grid,
+  } from "@mui/material";
 const LeftSideView = () => {
-    return <div className='column'>Some bla bla content</div>
-}
+  return <div className="column">Some bla bla content</div>;
+};
 
 const RightSideView = () => {
-    const {setActivePage} = useContext(AppContext);
+  const { setActivePage } = useContext(AppContext);
 
-   return <div className='column'>
-        <div>Upload it buddy</div>
-        <div>
-            <button onClick={(e) => {
-                setActivePage('application')
-            }}>Go to the second page</button>
-        </div>
-   </div>
-}
+  return (
+    <div className="column">
+      <div>Upload it buddy</div>
+      <div>
+        <button
+          onClick={(e) => {
+            setActivePage("application");
+          }}
+        >
+          Go to the second page
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export const Landing = (props) => {
-    // return <div className='container'>
-    //     <LeftSideView />
-    //     <RightSideView />
-    // </div>
+  // return <div className='container'>
+  //     <LeftSideView />
+  //     <RightSideView />
+  // </div>
 
-    return(
-        <Grid
-  container
-  direction="row"
-  justifyContent="space-between"
-  alignItems="center"
->
-    <Grid item style={{width: "inherit"}}>
-            <div style={{background: "#1A374D"}}>
+  return (
+    <Grid
+      container
+      direction="row"
+      justifyContent="space-between"
+      alignItems="center"
+    >
+      <Grid item style={{ width: "inherit" }}>
+        <div style={{ background: "#1A374D" }}></div>
+      </Grid>
+      <Grid item style={{ width: "inherit" }}>
 
-            </div>
+
+      </Grid>
     </Grid>
-    <Grid item style={{width: "inherit"}}>
-
-    </Grid>
-    
-</Grid>
-    )
-}
+  );
+};
