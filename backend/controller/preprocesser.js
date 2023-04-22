1d7b19effb414d7902159c4ba2ac7924356ae836
Sara María Bejarano <sm.bejarano44@uniandes.edu.co>
2023-04-22 02:36:00 +0100

    filter empty sections

--- backend/controller/preprocesser.js
+++ backend/controller/preprocesser.js
@@ -3,5 +3,9 @@ import {text} from "../model/constants.js";
 export function getSections(text) {
   // Split contents by '#'
   const sections = text.split('###');
+  sections.filter((section)=>{section!==""})
   return sections;
 }
+
+
+console.log(getSections(text)[4]);
