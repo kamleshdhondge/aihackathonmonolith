05e61402bf5dc9c4361ce17ca6a70dca79fab189
kamlesh <arvdigitech@gmail.com>
2023-04-22 08:50:21 +0100

    asd

--- frontend/src/components/Main.js
+++ frontend/src/components/Main.js
@@ -31,13 +31,18 @@ const FlagSection = (props) => {
 
 const LeftSideView = () => {
   const [summary, setSummary] =  useState(" Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed d eiusmod Lorem ipsum dolor sit amet, consectetur adipiscing elit, sedconsectetur adipiscing elit, sed do eiusmod");
-  const [flags, setFlags] =  useState({});
+  const [flags, setFlags] =  useState({data:{
+    "Green flags":[1],
+    "Orange flags" : [1],
+    "Red flags":[1]
+
+  }});
 
   const [open, setOpen] = useState(true);
   useEffect( () => {
   let data =  axios('http://localhost:3003/document/doc1/summary').then(async(data)=>{
-    console.log(data.data.summarize);
-    setSummary(data.data.summarize);
+    console.log(data.data.summary);
+    setSummary(data.data.summary);
     let flags = await axios('http://localhost:3003/document/doc1/flags');
     console.log(flags);
     setFlags(flags);
@@ -54,13 +59,6 @@ const LeftSideView = () => {
     <div className="report-container">
       <Card>
         <div className="report-content">
-          <h3>
-            Type of contract
-          </h3>
-          <div className="margin-bottom-15">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod
-          </div>
           <div className="margin-bottom-15" />
 
           <h3 className="margin-top-15">
@@ -70,11 +68,11 @@ const LeftSideView = () => {
           {summary}
           </div>
           <div className="margin-bottom-15" />
-          <FlagSection items={['1', '2', '3']} borderColor='#FF5A79' header='Red Flags' icon='red-flag.png' />
+          <FlagSection items={flags.data["Red flags"].map(data =>data.flag)} borderColor='#FF5A79' header='Red Flags' icon='red-flag.png' />
           <div className="margin-bottom-15" />
-          <FlagSection items={['1', '2', '3']} borderColor='#FFBD5A' header='Yellow Flags' icon='yellow-flag.png' />
+          <FlagSection items={flags.data["Orange flags"].map(data =>data.flag)} borderColor='#FFBD5A' header='Yellow Flags' icon='yellow-flag.png' />
           <div className="margin-bottom-15" />
-          <FlagSection items={['1', '2', '3']} borderColor='#70D77A' header='Green Flags' icon='green-flag.png' />
+          <FlagSection items={flags.data["Green flags"].map(data =>data.flag)} borderColor='#70D77A' header='Green Flags' icon='green-flag.png' />
           <div className="margin-bottom-15" />
           <h3 className="margin-top-15">
             This is our advice for you
