ed9d9501b8b5e2ecd0980ede6e1174ff62bb25c8
Juan Sebastian Prieto <js.prieto10@uniandes.edu.co>
2023-04-22 00:31:19 +0000

    add libraryes and tokens

--- backend/controller/document_controller.js
+++ backend/controller/document_controller.js
@@ -2,28 +2,15 @@
 import openai from 'openai';
 import { flags, summary } from "../model/constants.js";
 import {encode} from 'gpt-3-encoder';
+import { config } from 'dotenv';
 
-import openai from 'openai';
-
-const client = new openai.GPT3({
-  apiKey: 'sk-...vl6D',
-  model: 'text-davinci-002',
-});
+config()
 
 export function summarize(text) {
 
   let tokens = encode(text);
-
-  client.complete({
-    prompt: text,
-    maxTokens: tokens.length,
-  })
-    .then(response => {
-      return response.data.choices[0].text;
-    })
-    .catch(error => {
-      return(error);
-    });
+  let length = tokens.length;
+  return process.env.SECRET_KEY; 
 }
 
 export function getFlags(text) {
