1320db73ef78296c80e043db9d908898181b5380
Toño <aajn88@gmail.com>
2023-04-22 01:36:39 +0100

    Post method to send a new user message. Response gets bot reply

--- backend/routes/document_endpoint.js
+++ backend/routes/document_endpoint.js
@@ -4,14 +4,34 @@ import { summarize,getFlags } from "../controller/document_controller.js";
 
 const router = express.Router();
 
-router.route("/:id/chat").get(async (req, res) => {
-  const doc = getDocument(req.params.id);
-  res.json(doc.chatHistory);
-});
+router
+  .route("/:id/chat")
+  .get(async (req, res) => {
+    const doc = getDocument(req.params.id);
+    res.json(doc.chatHistory);
+  })
+  .post(async (req, res) => {
+    const doc = getDocument(req.params.id);
+    const lastMessage = doc.chatHistory[doc.chatHistory.length - 1];
+    const messageText = req.body.message;
+    const userMessage = {
+      id: lastMessage.id + 1,
+      send: "user",
+      message: messageText,
+    };
+    doc.chatHistory.push(userMessage);
+    const assistantMessage = {
+      id: userMessage.id + 1,
+      send: "assistant",
+      message: "Assume a new message goes here... :D",
+    };
+    doc.chatHistory.push(assistantMessage);
+    res.json(assistantMessage);
+  });
 
 router.route("/:id/summary").get(async (req, res) => {
   let text = getDocument(req.params.id).text;
-  res.json({summarize: summarize(text)});
+  res.json({ summarize: summarize(text) });
 });
 
 router.route("/:id/flags").get(async (req, res) => {
