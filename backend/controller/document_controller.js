e609a95cb2afec77fd5e8896fb22553a16ddd841
Juan Sebastian Prieto <js.prieto10@uniandes.edu.co>
2023-04-22 04:15:38 +0000

    more than sentence

--- backend/controller/document_controller.js
+++ backend/controller/document_controller.js
@@ -22,7 +22,7 @@ export async function summarize(text) {
 
   const sumariesNew = sumaries.join("\n")
   console.log(sumariesNew.length)
-  const prompt = "Create a summary with the following characteristics: in two sentences the summary should indicate what type of agreement it is, what the monthly rent is, what the address of the property is, when the initial term begins and ends, and the names of any key parties to the agreement \n" + sumariesNew
+  const prompt = "Create a summary with the following characteristics: the summary should indicate what type of agreement it is, what the monthly rent is, what the address of the property is, when the initial term begins and ends, and the names of any key parties to the agreement \n" + sumariesNew
   const maxTokens = 600
   const result = await chatCompletion([{
     role: "system",
