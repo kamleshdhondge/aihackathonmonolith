3c4c130e6115fa566cbda6fef6cc602c01b7b546
Juan Sebastian Prieto <js.prieto10@uniandes.edu.co>
2023-04-22 09:59:32 +0000

    summarize

--- backend/controller/document_controller.js
+++ backend/controller/document_controller.js
@@ -9,9 +9,9 @@ config();
 export async function summarize(text) {
   const sections = getSections(text);
   const summaries = await Promise.all(sections.map(async (section) => {
-    const prompt = "Use this section to answer questions:" + section + "\nQuestion: Create a summary of this text, which is a portion of a contract. The summary should include the details of the rental property, the duration of the lease, the rental amount and payment terms, security deposit, maintenance and repair responsibilities, restrictions on use if available. The summary should not exceed 300 hundred things .If you are unable to summarize or all the content is generic answer 'Nothing'. \n Answer:"
+    const prompt = "Use this contract section to answer questions:" + section + "\nQuestion: Create a summary. The summary should include the details of the rental property, the duration of the lease, the rental amount and payment terms, security deposit, maintenance and repair responsibilities, restrictions on use if available. The summary should not exceed 300 hundred things .If you are unable to summarize or all the content is generic answer 'Nothing'. \n Answer:"
     const maxTokens = MAX_TOKENS
-    const result = await completion(prompt, { temperature: 0, maxTokens, frequencyPenalty:2})
+    const result = await completion(prompt, { temperature: 0, maxTokens})
     return result
   }));
 
@@ -25,7 +25,7 @@ export async function summarize(text) {
     { role: "user", content: "Filter out all the non-important or generic sections." },
   ];
   const maxTokens = 2000
-  const result = await chatCompletion(messages, { temperature: 0, maxTokens, frequencyPenalty:1})
+  const result = await chatCompletion(messages, {temperature: 0, maxTokens, presencePenalty:-1 ,frequencyPenalty:2})
   return result
 }
 
